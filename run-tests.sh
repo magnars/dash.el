065f76b740243c87f498a19a6fd069c3d7c6400c
Magnar Sveen <magnars@gmail.com>
2012-09-24 23:26:58 +0200

    Use ert.el to run on Emacs 23 as well.

--- run-tests.sh
+++ run-tests.sh
@@ -4,4 +4,4 @@ if [ -z "$EMACS" ] ; then
     EMACS="emacs"
 fi
 
-$EMACS -batch -l ert -l bang.el -l tests.el -f ert-run-tests-batch-and-exit
+$EMACS -batch -l ert.el -l bang.el -l tests.el -f ert-run-tests-batch-and-exit

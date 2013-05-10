336e5a5da6510320989d2e154a2e7aa7eeb1c389
Magnar Sveen <magnars@gmail.com>
2013-05-10 16:59:37 +0200

    Release 1.3.1

--- dash.el
+++ dash.el
@@ -3,7 +3,7 @@
 ;; Copyright (C) 2012 Magnar Sveen
 
 ;; Author: Magnar Sveen <magnars@gmail.com>
-;; Version: 1.3.0
+;; Version: 1.3.1
 ;; Keywords: lists
 
 ;; This program is free software; you can redistribute it and/or modify

9abae5e6bc3b0f71f2bef38043796333559eaf00
Fuco1 <dota.keys@gmail.com>
2013-08-16 00:55:16 +0200

    Add combinators

--- create-docs.sh
+++ create-docs.sh
@@ -4,4 +4,4 @@ if [ -z "$EMACS" ] ; then
     EMACS="emacs"
 fi
 
-$EMACS -batch -l dash.el -l dev/examples-to-docs.el -l dev/examples.el -f create-docs-file
+$EMACS -batch -l dash-functional.el -l dash.el -l dev/examples-to-docs.el -l dev/examples.el -f create-docs-file

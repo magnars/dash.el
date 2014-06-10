f9e05b204c31aba0de1336e758457bf06cbe5196
Manuel Vázquez Acosta <mva.led@gmail.com>
2014-06-10 11:43:54 -0400

    Make `-reductions' to avoid the "first is nil".
    Now the following is possible:
    
      (-reductions #'+ '(1 2 3))
      => (1 3 6)

--- dash.el
+++ dash.el
@@ -202,14 +202,17 @@ A collecting reduce is a reduce that does not returns a single value (the
 result of the last computation) but a list of all computations made.
 
 The FORM should use 'FIRST' and 'SECOND' as the current items to be processed.
-The first time FORM is executed FIRST takes nil."
+If the list L does not have at least two items, FORM will not be used at all.
+The 'FIRST' variable will contain the first item of the list L the first time
+FORM is used, after that point 'FIRST' will contain the result from the
+previous computation."
   `(-flatten
      (-reduce-from
        (lambda (previous second)
 	 (let ((first (car (last previous))))
 	   (list previous ,form)))
-       ()
-       ,l)))
+       (list (car ,l))
+       (cdr ,l))))
 
 (defun -reductions(func l)
   "Functional form for `--reductions'.

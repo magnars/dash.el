174187faab2151c1f0fdf1a5e176f0fc4314e5e7
Magnar Sveen <magnars@gmail.com>
2015-10-21 10:09:46 +0200

    Add lexical binding pragma to dash.el
    **Please note** The lexical binding in this file is not utilised at the
    moment. We will take full advantage of lexical binding in an upcoming 2.0
    release of Dash. In the meantime, we've added the pragma to avoid a bug that
    you can read more about in issue #130

--- dash.el
+++ dash.el
@@ -1,4 +1,4 @@
-;;; dash.el --- A modern list library for Emacs
+;;; dash.el --- A modern list library for Emacs  -*- lexical-binding: t -*-
 
 ;; Copyright (C) 2012-2015 Free Software Foundation, Inc.
 
@@ -24,6 +24,12 @@
 ;; A modern list api for Emacs.
 ;;
 ;; See documentation on https://github.com/magnars/dash.el#functions
+;;
+;; **Please note** The lexical binding in this file is not utilised at the
+;; moment. We will take full advantage of lexical binding in an upcoming 2.0
+;; release of Dash. In the meantime, we've added the pragma to avoid a bug that
+;; you can read more about in https://github.com/magnars/dash.el/issues/130.
+;;
 
 ;;; Code:
 

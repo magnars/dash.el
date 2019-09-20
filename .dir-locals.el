7629dae97d6648ef560569067801a3b91028c1c0
Basil L. Contovounesios <contovob@tcd.ie>
2019-09-20 18:02:28 +0100

    Add dir-locals-file with useful settings
    * .dir-locals.el: New file.

---
+++ .dir-locals.el
@@ -0,0 +1,12 @@
+((nil
+  (bug-reference-bug-regexp . "\\(\\(?:issue\\|pr\\) ?\\)?#\\([[:digit:]]+\\)")
+  (bug-reference-url-format . "https://github.com/magnars/dash.el/issues/%s")
+  (fill-column . 70)
+  (sentence-end-double-space . t)
+  (tab-width . 8))
+ (emacs-lisp-mode
+  (indent-tabs-mode . nil)
+  (mode . bug-reference-prog))
+ (texinfo-mode
+  (indent-tabs-mode . nil)
+  (mode . bug-reference-prog)))

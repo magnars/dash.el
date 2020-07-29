feea52d128bce9b79329c948e4aae3cb511faebf
Leo Vivier <leo.vivier+dev@gmail.com>
2020-07-30 00:10:24 +0200

    Fix typo
    * dash.el (-some-->): Fix typo.
    
    Copyright-paperwork-exempt: yes

--- dash.el
+++ dash.el
@@ -1581,7 +1581,7 @@ and when that result is non-nil, through the next form, etc."
                  ,@more))))
 
 (defmacro -some--> (x &optional form &rest more)
-  "When expr in non-nil, thread it through the first form (via `-->'),
+  "When expr is non-nil, thread it through the first form (via `-->'),
 and when that result is non-nil, through the next form, etc."
   (declare (debug ->)
            (indent 1))

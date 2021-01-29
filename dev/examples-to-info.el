eedc2affad34471bd99b5fae56c6127c99eed3a2
Basil L. Contovounesios <contovob@tcd.ie>
2021-01-29 02:22:31 +0000

    Fix docstring Texinfo generation
    * dev/examples-to-info.el (format-docstring): Generalize @-quoting
    by doing it first.  Use \< and \> in place of \b for better word
    boundary detection.  Mark all replacements as fixed and literal.
    * dash.texi: Regenerate.

--- dev/examples-to-info.el
+++ dev/examples-to-info.el
@@ -94,10 +94,11 @@ Based on `describe-function-1'."
 (defun format-docstring (docstring)
   (let (case-fold-search)
     (--> docstring
-      (replace-regexp-in-string "\\b\\([A-Z][A-Z-]*[0-9]*\\)\\b" 'quote-and-downcase it t)
-      (replace-regexp-in-string "`\\([^ ]+\\)'" 'unquote-and-link it t)
-      (replace-regexp-in-string "{,@}" "{,@@}" it t)
-      (replace-regexp-in-string "^  " "    " it)
+      (replace-regexp-in-string "@" "@@" it t t)
+      (replace-regexp-in-string "\\<\\([A-Z][A-Z-]*[0-9]*\\)\\>"
+                                #'quote-and-downcase it t t)
+      (replace-regexp-in-string "`\\([^ ]+\\)'" #'unquote-and-link it t t)
+      (replace-regexp-in-string "^  " "    " it t t)
       (replace-regexp-in-string
        "\\.\\.\\.\\($\\)?"
        (lambda (_) (if (match-beginning 1) "@enddots{}" "@dots{}"))

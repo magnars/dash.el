2675596b9ac1c4b9d47b93e227f06f8ec6755ec6
Basil L. Contovounesios <contovob@tcd.ie>
2021-07-08 21:07:32 +0100

    Release Dash 2.19.0
    * dash.el: Bump to version 2.19.0.
    * README.md:
    * dash.texi: Regenerate docs.
    
    Closes #382.

--- dash.el
+++ dash.el
@@ -3,7 +3,7 @@
 ;; Copyright (C) 2012-2021 Free Software Foundation, Inc.
 
 ;; Author: Magnar Sveen <magnars@gmail.com>
-;; Version: 2.18.1
+;; Version: 2.19.0
 ;; Package-Requires: ((emacs "24"))
 ;; Keywords: extensions, lisp
 ;; Homepage: https://github.com/magnars/dash.el

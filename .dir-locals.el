da167c51e9fd167a48d06c7c0ee8e3ac7abd9718
Basil L. Contovounesios <contovob@tcd.ie>
2021-09-20 01:11:51 +0100

    Remove bug-reference setup from dir-locals-file
    * .dir-locals-file.el (bug-reference-bug-regexp)
    (bug-reference-url-format): Remove in preference of Emacs 28's more
    general DWIM setup.  Users on older Emacs versions can still add the
    relevant incantations to their personal .dir-locals-2.el file.

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,6 +1,4 @@
 ((nil
-  (bug-reference-bug-regexp . "\\(\\(?:issue\\|pr\\) ?\\)?#\\([[:digit:]]+\\)")
-  (bug-reference-url-format . "https://github.com/magnars/dash.el/issues/%s")
   (fill-column . 70)
   (sentence-end-double-space . t)
   (tab-width . 8))
